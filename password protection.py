a3e4ae9a76479620760835458d4579692d2910b5
Suman-K9 <107767311+Suman-K9@users.noreply.github.com>
2024-10-01 21:32:15 +0530

    Update password protection.py

--- password protection.py
+++ password protection.py
@@ -16,3 +16,24 @@ def encrypt_file(file_path, password):
         file.write(encrypted_key)
 
 encrypt_file('your_python_file.py', 'your_password')
+
+
+
+##### To run the file
+
+from cryptography.fernet import Fernet
+import base64
+
+def decrypt_file(file_path, password):
+    with open('key.txt', 'rb') as file:
+        encrypted_key = file.read()
+    key = base64.b64decode(encrypted_key)
+    cipher_suite = Fernet(key)
+    with open(file_path, 'rb') as file:
+        encrypted_data = file.read()
+    decrypted_data = cipher_suite.decrypt(encrypted_data)
+    with open(file_path[:-9], 'wb') as file:
+        file.write(decrypted_data)
+
+decrypt_file('your_python_file.py.encrypted', 'your_password')
+exec(open('your_python_file.py').read())
